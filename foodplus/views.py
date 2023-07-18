94b96c262027a25d193aca68a5d8cf15ea0eb559
kimberly558 <kimberlychumba19@gmail.com>
2023-07-18 13:23:06 +0300

    add test file

--- foodplus/views.py
+++ foodplus/views.py
@@ -13,17 +13,31 @@ def insert(request):
     if request.method == "POST":
         name = request.POST.get('name')
         school = request.POST.get('school')
-        email= request.POST.get('email')
+        email = request.POST.get('email')
 
         person = People(name=name, school=school)
         person.save()
 
         # print(name, school, email)
-    return HttpResponse("Success")
 
 
 def people(request):
     d = People.objects.all()
     return render(request, 'people.html', {
         "data": d
+    })
+
+
+def delete(request, id):
+    dd = People.objects.get(id=id)
+    dd.delete()
+
+    return HttpResponse("Delete Successfully")
+
+
+def update(request, id):
+    l = People.objects.get(id=id)
+
+    return render(request, "edit.html", {
+        "l": l
     })
